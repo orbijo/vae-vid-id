97b773f189224bc44c6636bbea45b6fd46b7e24e
Josh Arkane Orbiso <orbisojo@gmail.com>
2025-03-01 03:28:21 +0800

    in progress: changing straight download to -> download, rename, trim, and add caption txt file

--- vid_scraper.py
+++ vid_scraper.py
@@ -1,53 +1,47 @@
-### Scraper for Panda70m Dataset ###
-import yt_dlp
+### Scraper for Panda70m Dataset and Format it according to paper documentation (Vn.mp4 and Vn.txt (annotation))###
+from pytubefix import YouTube
 import pandas as pd
 import os
+import json
 
-# CSV Path
-filename = "panda70m_training_2m.csv"
-csv = "csv/" + filename
-df = pd.read_csv(csv)
+# CONFIG
+num_samples = 1 # number of samples
+filename = "panda70m_training_2m.csv" # data set csv filename
+directory = "data/" # csv directory
+
+# SETUP
+csv = directory + filename
+base_df = pd.read_csv(csv)
+df = base_df[['videoID', 'url', 'timestamp', 'caption']].head(num_samples) # Select only relevant columns
 
 # Folder for output (Following paper directory structure)
-# For videos
 base_dir_vid = "examples/videos"
-gt_dir_vid = os.path.join(base_dir_vid, "gt")  # Ground truth videos
-recon_dir_vid = os.path.join(base_dir_vid, "recon")  # Reconstructed videos
+gt_dir_vid = os.path.join(base_dir_vid, "gt")
+recon_dir_vid = os.path.join(base_dir_vid, "recon")
 os.makedirs(gt_dir_vid, exist_ok=True)
 os.makedirs(recon_dir_vid, exist_ok=True)
-print(f"Directories created:\n- {gt_dir_vid}\n- {recon_dir_vid}")
 
-### DECIDED NOT TO USE IMAGES ###
-# For images
-# base_dir_img = "examples/videos"
-# gt_dir_img = os.path.join(base_dir_img, "gt")  # Ground truth videos
-# recon_dir_img = os.path.join(base_dir_img, "recon")  # Reconstructed videos
-# os.makedirs(gt_dir_img, exist_ok=True)
-# os.makedirs(recon_dir_img, exist_ok=True)
-# print(f"Directories created:\n- {gt_dir_img}\n- {recon_dir_img}")
+def get_samples(video_id, url, timestamps, video_name):
+    yt = YouTube(url)
+    print(yt.title)
 
-def get_samples(video_id, url, timestamp):
-    video_path = os.path.join(gt_dir_vid, f"{video_id}.mp4")
-    trimmed_path = os.path.join(gt_dir_vid, f"{video_id}_trimmed.mp4")
+    # video_path = os.path.join(gt_dir_vid, f"{video_id}.mp4")
+    # trimmed_path = os.path.join(gt_dir_vid, f"{video_id}_trimmed.mp4")
 
-    if not os.path.exists(video_path):
-        ydl_opts = {"outtmpl":video_path}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+    # if not os.path.exists(video_path):
+    #     ydl_opts = {"outtmpl":video_path}
+    #     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #         ydl.download([url])
     
-    start_time, end_time = timestamp[0]
+    # start_time, end_time = timestamp[0]
 
-    os.system(f"ffmpeg -i {video_path} -ss {start_time} -to {end_time} -c:v libx264 -c:a aac {trimmed_path}")
+    # os.system(f"ffmpeg -i {video_path} -ss {start_time} -to {end_time} -c:v libx264 -c:a aac {trimmed_path}")
 
-    return trimmed_path
+    # return trimmed_path
 
-# Process first 50 videos
 for i, row in df.iterrows():
+    video_name = f"video{i+1}" # filename: video1.mp4, video1.txt, video2.mp4 ... etc
     video_id = row["videoID"]
     url = row["url"]
-    timestamps = eval(row["timestamp"])  # Convert string list to actual list
-    trimmed_video = get_samples(video_id, url, timestamps)
-
-    print(f"Processed {trimmed_video}")
-    if i >= 50:
-        break
+    timestamps = eval(row["timestamp"]) # timestamp in csv is a str list. convert to actual list
+    get_samples(video_id, url, timestamps, video_name)
